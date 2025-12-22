62a62575e1c3cedaf393ebc55d79a3c2f710c8a1
gull <gull@iue.tuwien.ac.at>
2025-12-22 09:39:04 +0100

    mace code public for easy deployment

---
+++ lib/config/credentials.dart
@@ -0,0 +1,5 @@
+class Config {
+  // Make sure this file is NOT committed to public repo
+  static const String admobBannerIdAndroid = 'ca-app-pub-3940256099942544/6300978111';
+  static const String admobBannerIdIOS = 'ca-app-pub-3940256099942544/2934735716';
+}
