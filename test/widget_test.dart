3ee7670f937963b72d0cc1a138a1339982777058
gull <gull@iue.tuwien.ac.at>
2025-12-22 14:58:13 +0100

    removed old names hopfully

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:edyn_quizz/main.dart';
+import 'package:Math_Quiz/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
